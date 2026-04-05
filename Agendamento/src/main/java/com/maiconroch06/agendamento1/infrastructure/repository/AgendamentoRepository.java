22d39a905aa21d66164c8a772e2a9730ab8b1f49
Maicon_Rocha <maiconpablo588@gmail.com>
2026-04-05 11:31:07 -0300

    Classe repository
    Classe onde o cod conversa com o banco de dados

---
+++ Agendamento/src/main/java/com/maiconroch06/agendamento1/infrastructure/repository/AgendamentoRepository.java
@@ -0,0 +1,29 @@
+package com.maiconroch06.agendamento1.infrastructure.repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.maiconroch06.agendamento1.infrastructure.model.Agendamento;
+
+import jakarta.transaction.Transactional;
+
+public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
+
+	// Buscar horarios reservados de um profissional
+	List<Agendamento> findByProfissionalAndDataHoraAgendamentoBetween(String profissional, LocalDateTime dataHoraInicio, LocalDateTime dataHoraFim);
+	
+	// Buscar horarios reservados de um cliente
+	List<Agendamento> findByClienteAndDataHoraAgendamentoBetween(String servico, LocalDateTime dataHoraInicio, LocalDateTime dataHoraFim);
+	
+	// Buscar horarios reservados entre intervalo de tempo
+	List<Agendamento> findByDataHoraAgendamentoBetween(LocalDateTime dataHoraInicio, LocalDateTime dataHoraFim);
+	
+	// Servico entre dois intervalo de tempo
+	List<Agendamento> findByServicoAndDataHoraAgendamentoBetween(String servico, LocalDateTime dataHoraInicio, LocalDateTime dataHoraFim);
+	
+	// Deletar horario de um cliente
+	@Transactional
+	void deleteByClienteAndDataHoraAgendamento(String cliente, LocalDateTime dataHoraAgendamento);
+}
