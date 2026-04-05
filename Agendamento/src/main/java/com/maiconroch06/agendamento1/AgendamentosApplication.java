3e7fe7d504106171a529e8e5691318817d3cbad9
Maicon_Rocha <maiconpablo588@gmail.com>
2026-04-05 11:29:14 -0300

    Projeto base em SpringBoot

---
+++ Agendamento/src/main/java/com/maiconroch06/agendamento1/AgendamentosApplication.java
@@ -0,0 +1,12 @@
+package com.maiconroch06.agendamento1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentosApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentosApplication.class, args);
+	}
+}
---
+++ Agendamento/src/test/java/com/maiconroch06/agendamento1/Agendamento1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maiconroch06.agendamento1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Agendamento1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
